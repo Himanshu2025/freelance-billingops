ec8d2f54cfd70f2be86939663c27a551f5b8a36e
Himanshu Kulkarni <hkul0004@student.monash.edu>
2026-03-10 17:36:06 +1100

    feat(webhooks): implement mock payment webhook to update invoice status

--- backend/billingops.Api/obj/Debug/net10.0/billingops.Api.AssemblyInfo.cs
+++ backend/billingops.Api/obj/Debug/net10.0/billingops.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("billingops.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbd9d4d2a3c07bed9eb27c602db7b8984f1e1aed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00fca124800397a64096dbe8f339c4a428c34891")]
 [assembly: System.Reflection.AssemblyProductAttribute("billingops.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("billingops.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
